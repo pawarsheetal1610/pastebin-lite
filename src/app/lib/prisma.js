7f31edf51376dce67312077210249e066ad82bb5
Sheetal Pawar <pawarsheetal1610@gmail.com>
2025-12-31 00:26:30 +0530

    Update prisma.js

--- src/app/lib/prisma.js
+++ src/app/lib/prisma.js
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = globalThis;
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
     log: ["error"],
   });
